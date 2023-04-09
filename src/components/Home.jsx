c0bfa36c5d412795b7c3e86603bfe3fe4d028cd1
1js2002 <dgoud99120@gmail.com>
2023-04-09 11:10:19 +0530

    half done

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import SwipeDrawer from './SwipeDrawer';
+import { styled } from '@mui/material/styles';
+function Home() {
+  return (
+    <Box style={{ display: 'flex', width: '100%' }}>
+      <SwipeDrawer/>
+
+    </Box>
+  )
+}
+
+export default Home
---
+++ src/components/notes/Notes.jsx
@@ -0,0 +1,52 @@
+import { styled } from '@mui/material/styles';
+
+import { Box } from "@mui/material"
+import Note from './Note';
+import { DataContext } from '../../context/DataProvider';
+
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    ...theme.mixins.toolbar,
+}));
+
+
+const Notes = () => {
+    return (
+        <Box sx={{ display: 'flex', width: '100%' }}>
+            <Box sx={{ p: 3, width: '100%' }}>
+                <DrawerHeader />
+                <Form />
+                { notes.length > 0 ? 
+                    <DragDropContext onDragEnd={onDragEnd}>
+                        <Droppable droppableId="droppable">
+                            {(provided, snapshot) => (
+                                <Grid container style={{ marginTop: 16}}
+                                    {...provided.droppableProps}
+                                    ref={provided.innerRef}
+                                >
+                                {
+                                    notes.map((note, index) => (
+                                        <Draggable key={note.id} draggableId={note.id} index={index}>
+                                            {(provided, snapshot) => (
+                                                <Grid ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    item
+                                                >
+                                                    <Note note={note} />
+                                                </Grid>
+                                            )}
+                                        </Draggable >
+                                    ))
+                                }
+                                </Grid>
+                            )}
+                        </Droppable >
+                    </DragDropContext>
+                : <EmptyNotes /> }
+            </Box>
+        </Box>
+    )
+}
+
+export default Notes;
